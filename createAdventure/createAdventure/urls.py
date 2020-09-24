7a8bf0596520d1df025306e8dd1dfb16324b732f
ph8isgoodforyou <ignasshallpass@gmail.com>
2020-09-24 03:25:45 +0300

    Added ability to register or login via google or fb account, started working on OAuth2

--- createAdventure/createAdventure/urls.py
+++ createAdventure/createAdventure/urls.py
@@ -1,19 +1,9 @@
 """createAdventure URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
+from django.http import request
 from django.urls import path, include
 from django.conf.urls import url
 from rest_framework_swagger.views import get_swagger_view
@@ -22,8 +12,13 @@ schema_view = get_swagger_view(title='Create Adventure API Documentation')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     # url(r'docs/', schema_view, name='swagger'),
-    path('', schema_view, name='swagger'),
+    path('', login_required(schema_view), name='swagger'),
+
+    # url(r'account/', include('allauth.urls')),
+    path('account/', include('allauth.urls')),
+
     path('', include('accommodation.urls')),
     path('', include('country.urls')),
     path('', include('goods.urls')),
@@ -32,3 +27,33 @@ urlpatterns = [
     path('', include('transport.urls')),
     path('', include('trip.urls')),
 ]
+
+"""
+            allauth provided urls
+--------------------------------------------------------
+    path("signup/", views.signup, name="account_signup"),
+    path("login/", views.login, name="account_login"),
+    path("logout/", views.logout, name="account_logout"),
+    path("password/change/", views.password_change,
+         name="account_change_password"),
+    path("password/set/", views.password_set, name="account_set_password"),
+    path("inactive/", views.account_inactive, name="account_inactive"),
+
+    # E-mail
+    path("email/", views.email, name="account_email"),
+    path("confirm-email/", views.email_verification_sent,
+         name="account_email_verification_sent"),
+    re_path(r"^confirm-email/(?P<key>[-:\w]+)/$", views.confirm_email,
+            name="account_confirm_email"),
+
+    # password reset
+    path("password/reset/", views.password_reset,
+         name="account_reset_password"),
+    path("password/reset/done/", views.password_reset_done,
+         name="account_reset_password_done"),
+    re_path(r"^password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
+            views.password_reset_from_key,
+            name="account_reset_password_from_key"),
+    path("password/reset/key/done/", views.password_reset_from_key_done,
+         name="account_reset_password_from_key_done"),
+"""
